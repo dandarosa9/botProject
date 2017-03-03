145151ca0e65daeee3c8b7701bec49d176438ff3
daro <daro@PC-DL172C>
2017-03-02 20:07:08 -0500

    Add project files.

---
+++ Bot Application1/Controllers/MessagesController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace OutlookBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                // calculate something for us to return
+                int length = (activity.Text ?? string.Empty).Length;
+
+                string eventString;
+                LUISobject eventLUIS = await GetEntityFromLUIS(activity.Text);
+                // return our reply to the user
+                Activity reply = activity.CreateReply($"You sent {activity.Text} which returned {eventLUIS}");
+                await connector.Conversations.ReplyToActivityAsync(reply);
+
+                // // return our reply to the user
+                // Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+                // await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private async Task<LUISobject> GetEntityFromLUIS(string Query)
+        {
+            Query = Uri.EscapeDataString(Query);
+            LUISobject Data = new LUISobject();
+            using (HttpClient client = new HttpClient())
+            {
+                string RequestURI = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/e997d2c3-c4b6-4521-a058-71da36b4b298?subscription-key=e0f7d2fb61e74b4a95e266b711e18660&verbose=true&q=" + Query;
+                HttpResponseMessage msg = await client.GetAsync(RequestURI);
+
+                if (msg.IsSuccessStatusCode)
+                {
+                    var JsonDataResponse = await msg.Content.ReadAsStringAsync();
+                    Data = JsonConvert.DeserializeObject<LUISobject>(JsonDataResponse);
+                }
+            }
+            return Data;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Bot Application1/LUISobject.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OutlookBot
+{
+
+    public class LUISobject
+    {
+        public string luis_schema_version { get; set; }
+        public string versionId { get; set; }
+        public string name { get; set; }
+        public string desc { get; set; }
+        public string culture { get; set; }
+        public Intent[] intents { get; set; }
+        public Entity[] entities { get; set; }
+        public Composite[] composites { get; set; }
+        public object[] closedLists { get; set; }
+        public string[] bing_entities { get; set; }
+        public Action[] actions { get; set; }
+        public Model_Features[] model_features { get; set; }
+        public object[] regex_features { get; set; }
+        public Utterance[] utterances { get; set; }
+    }
+
+    public class Intent
+    {
+        public string name { get; set; }
+    }
+
+    public class Entity
+    {
+        public string name { get; set; }
+        public string[] children { get; set; }
+    }
+
+    public class Composite
+    {
+        public string name { get; set; }
+        public string[] children { get; set; }
+    }
+
+    public class Action
+    {
+        public string actionName { get; set; }
+        public string intentName { get; set; }
+        public Channel channel { get; set; }
+        public Actionparameter[] actionParameters { get; set; }
+    }
+
+    public class Channel
+    {
+        public string Name { get; set; }
+        public string Method { get; set; }
+        public Setting[] Settings { get; set; }
+    }
+
+    public class Setting
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class Actionparameter
+    {
+        public string parameterName { get; set; }
+        public string entityName { get; set; }
+        public bool required { get; set; }
+        public string question { get; set; }
+        public string phraseListFeatureName { get; set; }
+    }
+
+    public class Model_Features
+    {
+        public string name { get; set; }
+        public bool mode { get; set; }
+        public string words { get; set; }
+        public bool activated { get; set; }
+    }
+
+    public class Utterance
+    {
+        public string text { get; set; }
+        public string intent { get; set; }
+        public Entity1[] entities { get; set; }
+    }
+
+    public class Entity1
+    {
+        public string entity { get; set; }
+        public int startPos { get; set; }
+        public int endPos { get; set; }
+    }
+
+}
