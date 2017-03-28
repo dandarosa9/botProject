512d46b6e3305151ebc61a27042677de5f2c36b9
dandarosa9 <dandarosa9@aol.com>
2017-03-28 19:09:18 -0400

    Created Service provider object class

---
+++ Bot Application1/Models/ServiceProviderObject.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OutlookBot
+{
+    public class ServiceProviderObject
+    {
+            public string startTime { get; set; }
+            public string endTime { get; set; }
+    }
+}
